065f53987c04f5abb3a6d2c3a0650d2705f59d69
Gibson <gibsonavilan@gmail.com>
2023-10-09 22:53:26 -0500

    tiempo dentro y fuera de casa funciona

---
+++ geofences.py
@@ -0,0 +1,120 @@
+from shapely.geometry import Point
+from connectDb import requestDatas
+import pymongo
+import json
+
+def get_geofences_from_db(dId):
+    host = "app.blipconnection.com" 
+    port = 27017  
+    username = "admin"  
+    password = "iotabserver"  
+    database_name = "iotab" 
+    client = pymongo.MongoClient(host, port, username=username, password=password)
+    db = client[database_name]
+    collection = db["devices"]
+
+    device=collection.find({"dId":dId})[0]
+    data= json.loads(device["data"])
+
+    if device["data"]=="": return []
+    if "config" not in data or data["config"]=="": return []
+    if "geofences" not in data["config"]:return []
+    return data["config"]["geofences"]
+
+def crear_geocerca(coordenadas_geocerca=None, diametro_metros=100):
+    if isinstance(coordenadas_geocerca, list) and len(coordenadas_geocerca) == 2:
+        # Si se proporciona una sola coordenada (latitud, longitud), crea una geocerca circular
+        center_latitude, center_longitude = coordenadas_geocerca
+        radio_distance = diametro_metros / 2.0
+
+        # Crear un punto para el centro del círculo
+        center_point = Point(center_latitude, center_longitude)
+        # Crear una circunferencia (geocerca circular) alrededor del centro
+        circunference = center_point.buffer(
+            radio_distance / 100000)  # Buffer en metros
+        return circunference
+    else:
+        raise ValueError(
+            "Se debe proporcionar una sola tupla de latitud y longitud para crear la geocerca."
+        )
+    
+def verify_geofence_in_out(geofence_object, coordenates_device):
+
+    # Crea un punto para representar las coordenadas del dispositivo
+    device_point = Point(coordenates_device)
+
+    # Comprueba si el punto está dentro de la geocerca
+    inside = geofence_object.contains(device_point)
+
+    if inside:
+        return True
+    elif not inside:
+        return False
+    
+def verify_geofences_general(device_data, current_coords):
+
+    # '*862095056194456': {'dId': '*862095056194456', 'exits_count': 0, 'last_geofence': '', 'geofences': [{'activated': 1, 'name': 'Home', 'coords': [-31.399601, -64.191109], 'diameter': 100, 'id': 1}]}
+    geofences_array=device_data["geofences"]
+    # Corrobora tener geocercas configuradas en la base de datos
+    if len(geofences_array)==0: return
+
+    # Crea el "objeto geocerca" que corresponde a cada geocerca de la terminal que está guardada en la bdd
+    for geofence in geofences_array:
+        geofence_obj=crear_geocerca(
+            geofence["coords"], geofence["diameter"]
+        )    
+
+
+        # Corrobora si el dispositivo entró en una geocerca (está dentro de una geocerca y antes no estaba dentro de ninguna o estaba dentro de otra)
+        if (
+            verify_geofence_in_out(
+                geofence_obj, current_coords) == True and
+                device_data["last_geofence"] != geofence["name"]
+             
+        ):
+            device_data["last_geofence"]=geofence["name"]
+
+        # # Corrobora si el dispositivo salió de una geocerca (está fuera de la geocerca y antes estaba dentro)
+        elif (
+            device_data["last_geofence"]==geofence["name"]
+                and verify_geofence_in_out(geofence_obj, current_coords)
+                == False
+            ):
+            device_data["last_geofence"]=""
+            device_data["exits_count"]=device_data["exits_count"]+1
+            # Si no se cumplen ninguno de los casos anteriores, no se agrega la propiedad "geofence_status" a la respuesta
+            # Por ende, la base de datos conservará su "geofence_status" anterior cuando se actualize
+# print(requestDatas()[0])
+
+
+def count_exits(all_entrances):
+    # print(all_entrances[0])
+    full_response={}
+    for entrance in all_entrances:
+        data=entrance["data"]
+        if data=="": continue
+
+        if entrance["dId"] not in full_response:
+            # print(dId)
+            full_response[entrance["dId"]]={
+                "dId":entrance["dId"],
+                "exits_count":0,
+                "last_geofence":"",
+                "geofences":get_geofences_from_db(entrance["dId"])
+            }
+
+        verify_geofences_general(full_response[entrance["dId"]], [entrance["lat"], entrance["lng"]])
+
+    min_response={
+        "exits_count":0,
+        "detail":[]
+    }
+    for response in full_response:
+        # print(response)
+        min_response["detail"].append({"dId":response, "exits_count":full_response[response]["exits_count"]})
+        min_response["exits_count"]=min_response["exits_count"]+full_response[response]["exits_count"]
+        
+    return min_response if len(min_response["detail"])>1 else min_response["detail"][0]
+
+
+print(count_exits(requestDatas('*869310069283953')))
--- timeInAndOutHome.py
+++ timeInAndOutHome.py
@@ -1,6 +1,7 @@
 from connectDb import requestDatas
 import json
 import datetime
+import time
 
 
 
@@ -9,32 +10,41 @@ def timeInAndOutHome(all_data, dId=None ):
         allEntries = [item for item in all_data if  item["dId"] == dId] 
     else:
         allEntries = all_data
-    count_in_home = 0
-    count_out_home = 0
 
-    for entry in allEntries:
-        data = json.loads(entry["data"])
-        if len(data["gps"]) and len(data["wifi_location"]) ==0 :
-            count_out_home+= entry["time"]
-         
-        elif len(data["wifi_location"]) and len(data["gps"]) ==0:
-            count_in_home+= entry["time"]
-           
     
-  
+    sorted_allEntries = sorted(allEntries, key=lambda x: x["time"])
 
+    tiempo_dentro_casa = 0  
+    tiempo_fuera_casa = 0
 
-    delta_time_in_home = datetime.timedelta(seconds=count_in_home)      
-    delta_time_out_home = datetime.timedelta(seconds=count_out_home)
-    result={
-        "in_home": f"{delta_time_in_home.seconds//3600}:{(delta_time_in_home.seconds%3600)//60}:{delta_time_in_home.seconds%3600%60}",
-        "out_home": f"{delta_time_out_home.seconds//3600}:{(delta_time_out_home.seconds%3600)//60}:{delta_time_out_home.seconds%3600%60}",
-    }      
-    return result
-# fecha_start = "2023-10-09 23:59:59"
-# fecha_end = "2023-10-08 00:00:00"
-# fecha_obj_start = datetime.datetime.strptime(fecha_start, "%Y-%m-%d %H:%M:%S")
-# fecha_obj_end = datetime.datetime.strptime(fecha_end, "%Y-%m-%d %H:%M:%S")
-# timestamp_unix_start = fecha_obj_start.timestamp()
-# timestamp_unix_end = fecha_obj_end.timestamp()
-print(timeInAndOutHome(requestDatas("*862095056169599")))
+# Paso 3: Recorre la lista de objetos y calcula el tiempo acumulado.
+    for i in range(len(sorted_allEntries) - 1):
+        objeto_actual = sorted_allEntries[i]
+        data = json.loads(objeto_actual["data"])
+
+        objeto_siguiente = sorted_allEntries[i + 1]
+      
+        tiempo_diferencia = objeto_siguiente["time"] - objeto_actual["time"]
+
+        if len(data["wifi_location"]):
+            tiempo_dentro_casa += tiempo_diferencia
+        else:
+            tiempo_fuera_casa += tiempo_diferencia
+
+# Paso 4: Convierte los tiempos acumulados a días, horas, minutos y segundos.
+    def milisegundos_a_dias_horas_minutos_segundos(milisegundos):
+        segundos, milisegundos = divmod(milisegundos, 1000)
+        minutos, segundos = divmod(segundos, 60)
+        horas, minutos = divmod(minutos, 60)
+        dias, horas = divmod(horas, 24)
+        return dias, horas, minutos, segundos
+
+    dias_dentro_casa, horas_dentro_casa, minutos_dentro_casa, segundos_dentro_casa = milisegundos_a_dias_horas_minutos_segundos(tiempo_dentro_casa)
+    dias_fuera_casa, horas_fuera_casa, minutos_fuera_casa, segundos_fuera_casa = milisegundos_a_dias_horas_minutos_segundos(tiempo_fuera_casa)
+
+# Paso 5: Imprime los resultados.
+    print("Tiempo acumulado dentro de casa:", int(dias_dentro_casa), "días,", int(horas_dentro_casa), "horas,", int(minutos_dentro_casa), "minutos,", int(segundos_dentro_casa), "segundos")
+    print("Tiempo acumulado fuera de casa:", int(dias_fuera_casa), "días,", int(horas_fuera_casa), "horas,", int(minutos_fuera_casa), "minutos,", int(segundos_fuera_casa), "segundos")
+print(timeInAndOutHome(requestDatas("*862095056194456")))
+# datetime.datetime.fromtimestamp(list_in[i]) - datetime.datetime.fromtimestamp(list_in[i + 1])
+
