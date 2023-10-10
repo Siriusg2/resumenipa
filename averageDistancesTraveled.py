d502b4227b5e3ebcb5abda33779b1d80637a2343
Gibson <gibsonavilan@gmail.com>
2023-10-10 17:17:59 -0500

    average distance chart ready

--- averageDistancesTraveled.py
+++ averageDistancesTraveled.py
@@ -2,7 +2,7 @@ import json
 from connectDb import requestDatas
 from datetime import datetime
 from utils.calculate_distance import calculate_distance
-
+import pandas as pd
 
 def averageDistancesTraveled(all_data, dId=None):
     data_trackers = list(all_data)
@@ -28,7 +28,7 @@ def averageDistancesTraveled(all_data, dId=None):
             data = json.loads(tracker["data"])
 
             if "gps" in data and data["gps"] != "":
-                tracker_id = tracker["dId"]
+                tracker_id = tracker["device_name"]
                 timestamp_unix = data["gps"]["time"]
                 timestamp = datetime.fromtimestamp(timestamp_unix / 1000)
                 distance_datetime = str(timestamp.date())
@@ -103,19 +103,29 @@ def averageDistancesTraveled(all_data, dId=None):
             total_distance_per_tracker[tracker_gps]["total"], 2
         )
 
-    print(total_distance_traveled_kms)
-    print(
-        "promedio", total_distance_traveled_kms / len(dict_distances_per_tracker.keys())
-    )
-    print("total de recorrido por rastreador", total_distance_per_tracker)
-
-    return {
-        "total_distance_traveled_kms": total_distance_traveled_kms,
-        "average_distance_traveled_kms": total_distance_traveled_kms
-        / len(dict_distances_per_tracker.keys()),
-        "total_distance_per_tracker": total_distance_per_tracker,
-    }
+    # print(total_distance_traveled_kms)
+    # print(
+    #     "promedio", total_distance_traveled_kms / len(dict_distances_per_tracker.keys())
+    # )
+    # print("total de recorrido por rastreador", total_distance_per_tracker)
+    return total_distance_per_tracker
+    # return {
+    #     "total_distance_traveled_kms": total_distance_traveled_kms,
+    #     "average_distance_traveled_kms": total_distance_traveled_kms
+    #     / len(dict_distances_per_tracker.keys()),
+    #     "total_distance_per_tracker": total_distance_per_tracker,
+    # }
 
 
 all_data = requestDatas("6515cd2bf2295200154f579e")
-averageDistancesTraveled(all_data)
+# print(averageDistancesTraveled(all_data))
+
+def convert_data_to_excel(data):
+    df = pd.DataFrame(data).T
+
+# Calcular la columna 'average'
+    df['average'] = df['total'] / df['count']
+
+    # Exportar el DataFrame a un archivo Excel
+    df.to_excel("excel_reports/averageDistancesTraveled.xlsx")
+convert_data_to_excel(averageDistancesTraveled(all_data))
