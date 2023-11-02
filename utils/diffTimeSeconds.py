637267851c82b307772be0f66a5999b8ea9d8993
siriusg2 <gibsonavilan@gmail.com>
2023-11-02 07:25:15 -0500

    v2 first commit

--- utils/diffTimeSeconds.py
+++
@@ -1,9 +0,0 @@
-def diffTimeSeconds(timestamp_inicio, timestamp_final):
-    diferencia_segundos = timestamp_final - timestamp_inicio
-
-# Convierte la diferencia de tiempo a un formato más legible (por ejemplo, días, horas, minutos, segundos)
-    dias = diferencia_segundos // 86400
-    horas = (diferencia_segundos % 86400) // 3600
-    minutos = (diferencia_segundos % 3600) // 60
-    segundos = diferencia_segundos % 60
-    return segundos
