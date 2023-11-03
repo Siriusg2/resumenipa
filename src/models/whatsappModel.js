57a49bcd7a4975f2c7c70f4ced82a370d329653d
siriusg2 <gibsonavilan@gmail.com>
2023-11-03 08:51:09 -0500

    querys to whatsapp works

--- src/models/whatsappModel.js
+++ src/models/whatsappModel.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
+
     message: String,
     timestamp: Date
 });
 
 const messagesSchema = new Schema({
+
     number: String,
-    messages: [{
-        message: String,
-        timestamp: Date
-    }]
+    messages: [messageSchema]
 });
 
-const whatsappModel = mongoose.model('whatsapp', messagesSchema);
+const whatsappModel = mongoose.model('whatsapp', messagesSchema, 'whatsapp');
 
 module.exports = whatsappModel;
