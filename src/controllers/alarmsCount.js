0e595e91932d51be11672216e822755d93a0a26c
siriusg2 <gibsonavilan@gmail.com>
2023-11-03 08:25:11 -0500

    adding mongoose like dependencie

--- src/controllers/alarmsCount.js
+++ src/controllers/alarmsCount.js
@@ -5,12 +5,12 @@
 // const devicesCollection = db.collection('devices')
 // const datasCollection = db.collection('datas')
 // const channelsCollection = db.collection('channels')
-const datasModel = require('../models/datasModel')
+const whatsappModel = require('../models/whatsappModel')
 
 
 
 const alarmsCount = async (channelId, startDate, endDate) => {
-    const allDevices = await datasModel.find({})
+    const allDevices = await whatsappModel.findOne({ _id: '6515d7a60b72141bbd0febac' })
 
     return allDevices
 }
